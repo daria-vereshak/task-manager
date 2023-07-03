cf373607f00bb7aa4602c49a0673e36b32e8db70
daria-vereshak <dariavereshak02@gmail.com>
2023-07-03 12:51:40 +0300

    fix auth-page display

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import styles from './styles/auth.module.css';
+import styles from './styles/auth.module.css';
 import Form from './form'
 
 
 const Auth = ({typeForm, login, password, error}) => {
   return(
-    <div className='auth'>
+    <div className={styles.wrap}>
       <Form type={typeForm} login={login} password={password} error={error}/>
     </div>
   )
--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './styles/form.module.css';
+import styles from "./styles/form.module.css";
 import FormInput from "./form-input";
 import FormButton from "./form-button";
 import { onSubmit } from "../utils/utils";
@@ -8,47 +8,85 @@ import { formToggle } from "../store/slices/authorization/authorization-slice";
 import { useSelector } from "react-redux";
 
 const Form = () => {
-  const form = useSelector(state => state.authorization);
+  const form = useSelector((state) => state.authorization);
 
   const dispatch = Dispatch();
 
   const onToggleButtonClicked = () => {
     let newType;
-    if (form.typeForm === 'reg') newType = 'auth';
-    else newType = 'reg'
+    if (form.typeForm === "reg") newType = "auth";
+    else newType = "reg";
     dispatch(
       formToggle({
         typeForm: newType,
-        login: '',
-        password: '',
-        error: '',
+        login: "",
+        password: "",
+        error: "",
       })
-    )
-  }
+    );
+  };
 
-  if (form.typeForm === "reg") {return (
-    <>
-    <form className={styles.auth}>
-      <h2 className={styles.title}>Регистрация</h2>
-      <FormInput type="text" placeholder="Логин" isRequired = {true} value={form.login} />
-      <FormInput type="password" placeholder="Пароль" isRequired = {true} value={form.password} />
-      <FormInput type="password" placeholder="Повторите пароль" isRequired = {true} value={form.password} />
-      <FormButton text = "Зарегистрироваться" onClick={event=>onSubmit('reg', dispatch, event)} />
-      </form>
-      <button className={styles.button} onClick={onToggleButtonClicked}>Уже зарегистрированы? Авторизуйтесь!</button>
-  </>
-  )}
-  else {return (
-    <>
-    <form className={styles.auth}>
-      <h2 className={styles.title}>Авторизация</h2>
-      <FormInput type="text" placeholder="Логин" isRequired = {true} value={form.login} />
-      <FormInput type="password" placeholder="Пароль" isRequired = {true} value={form.password} />
-      <FormButton text = "Войти" onClick={event=>onSubmit('auth', dispatch, event)} />
-      </form>
-      <button className={styles.button} onClick={onToggleButtonClicked}>В первый раз? Зарегистрируйтесь!</button>
-    </>
-  )}
-}
+  if (form.typeForm === "reg") {
+    return (
+      <div className={styles.auth}>
+        <form>
+          <h2 className={styles.title}>Регистрация</h2>
+          <FormInput
+            type="text"
+            placeholder="Логин"
+            isRequired={true}
+            value={form.login}
+          />
+          <FormInput
+            type="password"
+            placeholder="Пароль"
+            isRequired={true}
+            value={form.password}
+          />
+          <FormInput
+            type="password"
+            placeholder="Повторите пароль"
+            isRequired={true}
+            value={form.password}
+          />
+          <FormButton
+            text="Зарегистрироваться"
+            onClick={(event) => onSubmit("reg", dispatch, event)}
+          />
+        </form>
+        <button className={styles.button} onClick={onToggleButtonClicked}>
+          Уже зарегистрированы? Авторизуйтесь!
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.auth}>
+        <form>
+          <h2 className={styles.title}>Авторизация</h2>
+          <FormInput
+            type="text"
+            placeholder="Логин"
+            isRequired={true}
+            value={form.login}
+          />
+          <FormInput
+            type="password"
+            placeholder="Пароль"
+            isRequired={true}
+            value={form.password}
+          />
+          <FormButton
+            text="Войти"
+            onClick={(event) => onSubmit("auth", dispatch, event)}
+          />
+        </form>
+        <button className={styles.button} onClick={onToggleButtonClicked}>
+          В первый раз? Зарегистрируйтесь!
+        </button>
+      </div>
+    );
+  }
+};
 
 export default Form;
